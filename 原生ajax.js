1ebdb99a62a714678affafc2320bed224cf94a21
shixiaozheng <1139375659@qq.com>
2019-09-18 10:20:44 +0800

    first

---
+++ 原生ajax.js
@@ -0,0 +1,10 @@
+let xhr = XMLHttpRequest()
+xhr.open()//请求方式  地址  是否异步
+xhr.send()
+xhr.onreadystatechange = function () {
+    if (xhr.readyState == 4) {
+        if (xhr.status == 200) {
+            console.log(xhr.responseText);
+        }
+    }
+}
